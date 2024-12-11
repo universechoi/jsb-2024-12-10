75c5651e3469871bd96cfd2bb7054664efe22b7b
universechoi <skye.of.universe@gmail.com>
2024-12-11 15:45:48 +0900

    ResponseBody

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,19 +2,22 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void home() {
+    @ResponseBody
+    public int home() {
         System.out.println("home");
-        this.contact();
+        return 22;
     }
 
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about");
-        this.contact();
+        return "안녕하세요.";
     }
 
     public void contact() {

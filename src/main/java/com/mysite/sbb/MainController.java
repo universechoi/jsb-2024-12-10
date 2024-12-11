05bae147f2ed3364d6207bcbf5b3213d9c495fb5
universechoi <skye.of.universe@gmail.com>
2024-12-11 15:45:02 +0900

    no action method name is must be readable

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -5,15 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    // GET http://localhost:8080/
     @GetMapping("/")
     public void home() {
         System.out.println("home");
+        this.contact();
     }
 
-    // GET http://localhost:8080/about
     @GetMapping("/about")
     public void about() {
         System.out.println("about");
+        this.contact();
+    }
+
+    public void contact() {
+        System.out.println("contact");
     }
 }

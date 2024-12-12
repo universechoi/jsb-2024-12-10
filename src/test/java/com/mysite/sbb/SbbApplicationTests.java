83c1107b2e99fe199cf4a1d316bee010592a47be
universechoi <skye.of.universe@gmail.com>
2024-12-12 16:43:52 +0900

    deleteTest

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -77,4 +77,14 @@ class SbbApplicationTests {
 		q.setSubject("수정된 제목");
 		this.questionRepository.save(q);
 	}
+
+	@Test
+	void deleteTest() {
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
 }

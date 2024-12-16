dc50e5efa31c5599c0d57709ce8793e2fce3e1fe
universechoi <skye.of.universe@gmail.com>
2024-12-16 18:08:00 +0900

    refactor detail

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -26,7 +26,7 @@ public class QuestionController {
     @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
-        model.addAttribute("question, question");
+        model.addAttribute("question", question);
         return "question_detail";
     }
 }
